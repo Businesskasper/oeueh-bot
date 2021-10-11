47a978dbc5cf24b1cddd1b968e45af8cf9458648
Max Jung <max.jung97@web.de>
2021-10-11 23:46:41 +0200

    feat: added nodemon for dev enviroment

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 require('dotenv').config();
-import { Bot } from "./bot";
-import container from "./inversify.config";
-import { TYPES } from "./types";
+import { Bot } from './bot';
+import container from './inversify.config';
+import { TYPES } from './types';
 
 let bot = container.get<Bot>(TYPES.Bot);
 
-bot.listen()
-  .then(_ => console.log('Logged in!'))
-  .catch(error => console.error(error))
+// test
+bot
+  .listen()
+  .then((_) => console.log('Logged in!'))
+  .catch((error) => console.error(error));
