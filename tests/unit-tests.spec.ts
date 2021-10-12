70b89dd506d673cf3095f94cc55b96ba3e2fac2c
Luka WEIS <luka.weis@vinci-energies.net>
2021-10-12 17:41:48 +0200

    Added tests for message broker

--- tests/unit-tests.spec.ts
+++ tests/unit-tests.spec.ts
@@ -3,6 +3,8 @@ import 'mocha';
 import { Message } from 'discord.js';
 import { instance, mock, verify, when } from 'ts-mockito';
 import { MappedResponder } from '../src/message-handler/mapped-responder/mapped-responder';
+import { MessageBroker } from '../src/message-broker';
+import { expect } from 'chai';
 
 describe('MappedResponder', () => {
   let mappedResponder: MappedResponder;
@@ -30,5 +32,34 @@ describe('MappedResponder', () => {
 
     mappedResponder.Handle(mockedMessageInstance);
     verify(mockedMessageClass.reply('pong!')).never();
+  });
+});
+
+describe('MessageBroker', () => {  
+  let messageBroker: MessageBroker = new MessageBroker();
+  let mockedMessageClass = mock(Message);
+  let mockedMessageInstance = instance(mockedMessageClass);
+  mockedMessageInstance.content = 'ping!';
+
+  let mockedReceivedMessageClass = mock(Message);
+  let mockedReceivedMessageInstance = instance(mockedReceivedMessageClass);
+
+  let sentMessage: string;
+
+  messageBroker.onMessageReceived$.subscribe((message: Message) => mockedReceivedMessageInstance = message)
+
+  messageBroker.onSendMessage$.subscribe((messageContent: string) => sentMessage = messageContent);
+
+  before(() => {
+    messageBroker.dispatchMessageReceived(mockedMessageInstance);
+    messageBroker.dispatchSendMessage('pong!')
+  })
+
+  it('should receive message', () => {
+    expect(mockedReceivedMessageInstance.content === 'ping!').to.be.true;
+  });
+
+  it('should send message', () => {
+    expect(sentMessage === 'pong!').to.be.true;
   });
 });
