9be69784b5ec62f7b0f3641c6147e7070adf6923
Luka Weis <LukaT.Weis@gmail.com>
2021-10-14 22:44:33 +0200

    added integration test

--- tests/integration-tests.spec.ts
+++ tests/integration-tests.spec.ts
@@ -1,25 +1,48 @@
 import "reflect-metadata";
 import 'mocha';
-import { instance, mock, verify, when } from "ts-mockito";
-import { Client } from "discord.js";
+import { instance, mock, spy, verify, when } from "ts-mockito";
+import { Client, Message } from "discord.js";
 import { Bot } from '../src/bot';
 import container from "../src/inversify.config";
 import { TYPES } from "../src/types";
+import { MessageBroker } from "../src/message-broker";
+import { expect } from "chai";
 
 
 describe('Bot', () => {
-    let discordMock: Client;
-    let discordInstance: Client;
+    let clientMock: Client;
+    let clientInstance: Client;
+    let client: Client;
     let bot: Bot;
 
+    let messageBrokerInstance: MessageBroker;
+    let messageBrokerMock: MessageBroker;
+
+    let messageMock: Message;
+    let messageInstance: Message;
+
     beforeEach(() => {
-        discordMock = mock(Client);
-        discordInstance = instance(discordMock);
+        clientMock = mock(Client);
+        clientInstance = instance(clientMock);
         container.rebind<Client>(TYPES.Client)
-            .toConstantValue(discordInstance);
-        bot = container.get<Bot>(TYPES.Bot);
+            .toConstantValue(clientInstance);
+
+        messageBrokerMock = mock(MessageBroker)
+        messageBrokerInstance = instance(messageBrokerMock);
+        container.rebind<MessageBroker>(TYPES.MessageBroker)
+            .toConstantValue(messageBrokerInstance);
+
+        client = container.get<Client>(TYPES.Client);
     });
 
     // Test cases here
+    it('Should dispatch SendMessage', () => {   
+        messageMock = mock(Message);
+        messageInstance = instance(messageMock);
+        messageInstance.content = 'ping!';
+        client.emit('message', messageInstance);
 
+        
+        verify(messageBrokerMock.dispatchMessageReceived(messageInstance)).once();
+    })
 });
